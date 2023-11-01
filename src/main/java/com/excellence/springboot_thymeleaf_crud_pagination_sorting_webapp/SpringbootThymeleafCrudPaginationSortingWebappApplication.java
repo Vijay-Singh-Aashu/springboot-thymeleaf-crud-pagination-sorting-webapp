9e269d4eaebbf23b97b1c503ff464d938281cc54
Vijay Singh <vijaysinghaashu@gmail.com>
2023-11-01 12:10:24 +0530

    base-code-created

---
+++ src/main/java/com/excellence/springboot_thymeleaf_crud_pagination_sorting_webapp/SpringbootThymeleafCrudPaginationSortingWebappApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot_thymeleaf_crud_pagination_sorting_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudPaginationSortingWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafCrudPaginationSortingWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/springboot_thymeleaf_crud_pagination_sorting_webapp/SpringbootThymeleafCrudPaginationSortingWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.springboot_thymeleaf_crud_pagination_sorting_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudPaginationSortingWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
